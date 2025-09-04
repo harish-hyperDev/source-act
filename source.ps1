6bedd675b6c431880dd8eafcafda602c637b04c4
harish-hyperDev <coolprogrammer.777@gmail.com>
2025-09-04 05:35:41 +0530

    added source-like script for PowerShell

---
+++ source.ps1
@@ -0,0 +1,23 @@
+﻿# set-alias source Source-Profile
+
+$EXPORT_SOURCE = "F:\.venv"
+
+function source {
+    param($activate_env)
+    $profile = $activate_env
+    if ($profile -eq "-l" -or $profile -eq 0) {
+        ls $EXPORT_SOURCE | select "Name"
+    }
+    else {
+        $source_path = "$EXPORT_SOURCE\$profile\Scripts\Activate.ps1"
+        . $source_path
+        echo $source_path
+    }
+}
+
+if (!$args[0]) {
+    ls $EXPORT_SOURCE | select "Name"
+
+} else {
+    source $args[0]
+}
